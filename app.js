1b45784807731b004774dbd916f74425e5edeb9e
hrshainik <hrsshainik@gmail.com>
2021-08-04 11:44:35 +0600

    add static html file

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const errorHandler = (err, req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use(errorHandler);
 
 mongoose
@@ -33,12 +34,6 @@ mongoose
   )
   .then(() => console.log("connected"))
   .catch((err) => console.log(err));
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Hello from the notes api!",
-  });
-});
 
 app.use("/api/v1/notes", noteRouter);
 app.use("/api/v1", userRouter);
